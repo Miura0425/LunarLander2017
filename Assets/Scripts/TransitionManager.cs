cf2a32a0ca7e56c70538f1a09d1d40d2337e9185
YukiMiura <miura@alfasystem.co.jp>
2017-05-19 14:27:29 +0900

    TransitionManagerの作成 TitleとMainGameシーンを作成し、ゲームの流れを構築

---
+++ Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public enum GAME_SCENE
+{
+	TITLE=0,
+	MAINGAME,
+};
+
+public class TransitionManager : MonoBehaviour {
+	// スタティックインスタンス
+	static private TransitionManager instance;
+	static public TransitionManager Instance {
+		get {
+			return instance;
+		}
+	}
+	/*---------------------------------------------------------------------*/
+	void Awake()
+	{
+		// インスタンスが存在するなら自身を削除する。
+		if (instance != null) {
+			Destroy (this.gameObject);
+		} else { // インスタンスが存在しないなら自信を登録する。
+			instance = this;
+		}
+
+		// シーンのロード時に削除されないように設定する。
+		DontDestroyOnLoad (this);
+	}
+
+	/// <summary>
+	/// シーンを切り替える
+	/// </summary>
+	public void ChangeScene(GAME_SCENE scene)
+	{
+		SceneManager.LoadScene ((int)scene);
+	}
+	/*---------------------------------------------------------------------*/
+	// テストコード
+	/*
+	public GAME_SCENE nextscene;
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			ChangeScene (nextscene);
+		}
+	}
+	*/
+	/*---------------------------------------------------------------------*/
+}
