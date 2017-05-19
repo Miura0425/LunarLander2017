6da2a4e019e9498b722498d3d90e52e03cf1f6e5
YukiMiura <miura@alfasystem.co.jp>
2017-05-19 15:39:27 +0900

    タイトルシーンの作成 TitleManagerの作成 入力処理 シーン遷移 Pressメッセージの作成 点滅処理

---
+++ Assets/Scripts/PressMsg.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PressMsg : MonoBehaviour {
+	
+	private Graphic MsgImg;
+	private float fNextTime=0;
+	[SerializeField]
+	private float fIntervalTime=0;
+	/*---------------------------------------------------------------------*/
+	void Awake () {
+		MsgImg = GetComponent<Graphic> ();
+		fNextTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FlashingMsg ();
+	}
+
+	private void FlashingMsg()
+	{
+		if (Time.time > fNextTime) {
+			MsgImg.enabled = !MsgImg.enabled;
+			fNextTime += fIntervalTime;
+		}
+	}
+	/*---------------------------------------------------------------------*/
+}
---
+++ Assets/Scripts/TitleManger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManger : MonoBehaviour {
+	[SerializeField,TooltipAttribute("スタートするためのキー")]
+	private KeyCode StartKey;
+
+	/*---------------------------------------------------------------------*/
+	// 更新処理
+	void Update () {
+		CheckStartKey ();
+	}
+	/// <summary>
+	/// スタートキーが押されたかチェックする。
+	/// </summary>
+	private void CheckStartKey()
+	{
+		if(Input.GetKeyDown(StartKey))
+		{
+			// メインゲームへ遷移する。
+			TransitionManager.Instance.ChangeScene(GAME_SCENE.MAINGAME);
+		}
+	}
+	/*---------------------------------------------------------------------*/
+}
