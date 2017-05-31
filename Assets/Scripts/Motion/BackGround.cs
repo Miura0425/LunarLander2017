2ecaa0cd07742d11cdaba6262ef88733956bfe58
YukiMiura <miura@alfasystem.co.jp>
2017-05-31 13:47:56 +0900

    タイトルシーンのLander作成 背景作成　適用済み

---
+++ Assets/Scripts/Motion/BackGround.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BackGround : MonoBehaviour {
+	// 判別用タイプ
+	private enum TYPE
+	{
+		SPRITE,	// スプライト
+		IMAGE, // イメージ
+	}
+	private TYPE type;
+
+	[SerializeField]
+	private SpriteRenderer _sprite;	// SpriteRenderer
+	[SerializeField]
+	private Image _image;			// Image
+
+	[SerializeField]
+	private float Speed;			// 速さ
+	private float fStartTime;		// 開始時間
+	[SerializeField][Range(0,1)]
+	private float Max_RGB;			// カラーRGB 最大値
+	[SerializeField][Range(0,1)]
+	private float Min_RGB;			// カラーRGB 最小値
+	private Color _color = new Color(1,1,1,1); // 変更用カラー
+	/*---------------------------------------------------------------------*/
+	void Awake () {
+		// SpriteRendererとImageどちらが登録されているかチャックして、タイプを設定する。
+		if (_sprite != null && _image == null) {
+			type = TYPE.SPRITE;
+		} else if (_image != null && _sprite == null) {
+			type = TYPE.IMAGE;
+		} else {
+			Debug.LogError ("SpriteRendererとImageの両方が設定されています。");
+		}
+	}
+
+	void Start()
+	{
+		// 初回リセット
+		Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ColorUpdate ();
+	}
+	/*---------------------------------------------------------------------*/
+	/// <summary>
+	/// 処理リセット処理
+	/// </summary>
+	private void Reset()
+	{
+		fStartTime = Time.timeSinceLevelLoad;
+	}
+	/// <summary>
+	/// 色を徐々に変更し、また戻していく処理
+	/// </summary>
+	private void ColorUpdate()
+	{
+		// 色更新処理
+		float diff = Time.timeSinceLevelLoad - fStartTime;
+		if (diff > Speed*2) {
+			Reset ();
+		}
+		float rate = diff / (Speed*2);
+		if (diff < Speed) {
+			_color.r = Mathf.Lerp (Min_RGB, Max_RGB, rate);
+			_color.g = Mathf.Lerp (Min_RGB, Max_RGB, rate);
+			_color.b = Mathf.Lerp (Min_RGB, Max_RGB, rate);
+		} else {
+			_color.r = Mathf.Lerp (Max_RGB, Min_RGB, rate);
+			_color.g = Mathf.Lerp (Max_RGB, Min_RGB, rate);
+			_color.b = Mathf.Lerp (Max_RGB, Min_RGB, rate);
+		}
+		if (type == TYPE.IMAGE) {
+			_image.color = _color;
+		} else if (type == TYPE.SPRITE) {
+			_sprite.color = _color;
+		}
+	}
+}
--- Assets/Scripts/Motion/Title_Lander.cs
+++ Assets/Scripts/Motion/Title_Lander.cs
@@ -2,99 +2,121 @@
 using System.Collections;
 
 public class Title_Lander : MonoBehaviour {
+
 	[SerializeField]
-	private GameObject[] points;
+	private Camera _Camera;	// カメラ
 
-	private GameObject targetObj = null;
+	private Vector3 vStartPoint; // 移動開始位置
+	private Vector3 vGoalPoint;	// 移動先位置
 
-	private bool isMove = false;
-	private bool isRot = false;
+	private bool isMove = false; // 移動フラグ
+	private bool isWait = false; // 待ちフラグ
 
-	private Vector3 startPos;
-	private Vector3 targetPos;
-	private Quaternion startRot;
-	private Quaternion targetRot;
-
-	private float fStartTime;
 	[SerializeField]
-	private float fMoveTime;
+	private float fMoveTime;		// 移動にかける時間
+	private float fMoveStartTime;	// 移動開始時間
 	[SerializeField]
-	private float fRotTime;
+	private float fWaitTime;		// 待ち時間
+	private float fWaitStartTime;	// 待ち開始時間
+	[SerializeField]
+	private int CutNum;	// 範囲分割数
 	/*---------------------------------------------------------------------*/
-	void Awake()
+	void Start()
 	{
-		Goal ();
+		SelectPoint ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Rotation ();
-		Move ();
-	}
-	void FixedUpdate()
-	{
-		
+		WaitUpdate ();
+		MoveUpdate ();
 	}
 	/*---------------------------------------------------------------------*/
-	private void CheckUseRocket()
+	/// <summary>
+	/// 移動開始位置と移動先位置を設定する。
+	/// </summary>
+	private void SelectPoint()
 	{
-		
-	}
-	private void CheckMove()
-	{
-		
-	}
-	private void CheckRotation()
-	{
-		
+		// 左上座標の取得
+		Vector2 lefttop = _Camera.ScreenToWorldPoint (Vector3.zero) ;
+		lefttop.Scale (new Vector2(1,-1)); // 上下反転
+		lefttop.x -= this.transform.localScale.x;
+		lefttop.y += this.transform.localScale.y;
+		// 右下座標の取得
+		Vector2 rightbottom = _Camera.ScreenToWorldPoint (new Vector3(Screen.width,Screen.height,0));
+		rightbottom.Scale (new Vector2 (1, -1)); // 上下反転
+		rightbottom.x += this.transform.localScale.x;
+		rightbottom.y -= this.transform.localScale.y;
+
+		// X軸Y軸の長さを取得
+		float lenX = rightbottom.x - lefttop.x;
+		float lenY = lefttop.y - rightbottom.y;
+
+		// 左端の位置のY座標をランダムに設定
+		int leftIdx = Random.Range (0, CutNum+1);
+		vStartPoint = new Vector3 ();
+		vStartPoint.x = lefttop.x;
+		vStartPoint.y = lefttop.y - ((lenY / CutNum)*leftIdx);
+
+		// 右端の位置のY座標をランダムに設定
+		int rightIdx = Random.Range (0, CutNum);
+		vGoalPoint = new Vector3 ();
+		vGoalPoint.x = rightbottom.x;
+		vGoalPoint.y = rightbottom.y + ((lenY / CutNum)*rightIdx);
+
+		// 移動開始位置を左右どちらにするかランダムに設定
+		if (Random.Range (0, 2) == 0) {
+			Vector3 tmp = vStartPoint;
+			vStartPoint = vGoalPoint;
+			vGoalPoint = tmp;
+		}
+
+		// 移動の準備
+		Ready ();
 	}
 	/*---------------------------------------------------------------------*/
-	private void Move()
+	/// <summary>
+	/// 移動開始前の準備処理
+	/// </summary>
+	private void Ready()
 	{
-		if (isMove) {
-			float diff = Time.timeSinceLevelLoad - fStartTime;
-			if (diff >= fMoveTime) {
+		this.transform.position = vStartPoint;
+		// 移動先方向を向く
+		Vector3 diff = vGoalPoint - this.transform.position;
+		this.transform.rotation = Quaternion.FromToRotation (Vector3.up, diff);
+		// 移動開始
+		fMoveStartTime = Time.timeSinceLevelLoad;
+		isMove = true;
+	}
+	/*---------------------------------------------------------------------*/
+	/// <summary>
+	/// 移動更新処理
+	/// </summary>
+	private void MoveUpdate()
+	{
+		if (isMove == true) {
+			float diff = Time.timeSinceLevelLoad - fMoveStartTime;
+			if (diff > fMoveTime) {
 				isMove = false;
-				Goal ();
+				isWait = true;
+				fWaitStartTime = Time.timeSinceLevelLoad;
 			}
 			float rate = diff / fMoveTime;
-			this.transform.position = Vector3.Lerp (startPos, targetPos, rate);
+			this.transform.position = Vector3.Lerp (vStartPoint, vGoalPoint, rate);
 		}
 	}
-	private void Rotation()
+	/// <summary>
+	/// 待ち更新処理
+	/// </summary>
+	private void WaitUpdate()
 	{
-		if (isRot == true) {
-			float diff = Time.timeSinceLevelLoad - fStartTime;
-			if (diff >= fRotTime) {
-				isRot = false;
-				MoveStart ();
+		if (isWait == true) {
+			float diff = Time.timeSinceLevelLoad - fWaitStartTime;
+			if (diff > fWaitTime) {
+				isWait = false;
+				SelectPoint ();
 			}
-			float rate = diff / fRotTime;
-			this.transform.rotation = Quaternion.Lerp (startRot, targetRot, rate);
 		}
-	}
-	private void Goal()
-	{
-		isMove = false;
-		int nextIdx = Random.Range (0, points.Length);
-		targetObj = points [nextIdx];
-
-		startRot = this.transform.rotation;
-		Vector3 vec = targetObj.transform.position - this.transform.position;
-		vec.z = 0;
-		targetRot = Quaternion.LookRotation(vec);
-
-
-		fStartTime = Time.timeSinceLevelLoad;
-
-		isRot = true;
-	}
-	private void MoveStart (){
-		isMove = true;
-		startPos = this.transform.position;
-		targetPos = targetObj.transform.position;
-
-		fStartTime = Time.timeSinceLevelLoad;
 	}
 	/*---------------------------------------------------------------------*/
 
