36ba9dbe60074d9ddc653455b981e57aa0080362
YukiMiura <miura@alfasystem.co.jp>
2017-06-02 10:02:40 +0900

    画面解像度を９６０：５４０に固定する。

---
+++ Assets/Scripts/GameInit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInit  {
+	[RuntimeInitializeOnLoadMethod]
+	static void OnRuntimeMethodLoad()
+	{
+		Screen.SetResolution (960, 540, false, 60);
+	}
+
+}
