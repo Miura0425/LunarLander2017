22b11dcd1d9aeba654ac52663966c51bb070c5ba
YukiMiura <miura@alfasystem.co.jp>
2017-08-04 17:33:09 +0900

    ユーザーアカウントページの作成開始

---
+++ Assets/Scripts/Controller/UserPage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UserPage : MonoBehaviour {
+	[SerializeField]
+	Image _BackGround;
+	[SerializeField]
+	Text _TitleText;
+	[SerializeField]
+	Text _UserName;
+	[SerializeField]
+	Text _HighScore;
+	[SerializeField]
+	Text _HighStage;
+
+	/* プレイ履歴オブジェクト */
+}
